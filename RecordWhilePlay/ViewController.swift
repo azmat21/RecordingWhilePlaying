661a11fd4386a45aaf6ea51a722214c1dda05462
ezmet <ezmet@otkur.biz>
2015-11-29 17:02:04 +0800

    Initial Commit

---
+++ RecordWhilePlay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecordWhilePlay
+//
+//  Created by Ezmet on 11/29/15.
+//  Copyright © 2015 Ezmet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
